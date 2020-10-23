8e9647c4975e8d39a5d2fbb9b69370d41f9e9b85
Catastor <71878166+Catastor@users.noreply.github.com>
2020-10-23 10:11:15 +0900

    バリデーションの修正
    忘れ物

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,16 +8,16 @@ class User < ApplicationRecord
   has_many :comments
   has_many :buyers
 
-  validates :nickname, presence: true, length: {maximum: 40} 
-  validates :email, format: { with: /\A[\w+-.]+@[a-z\d-.]+.[a-z]+\z/i }
-  validates :password, format: { with: /\A[a-zA-Z\d]+\z/ }, length: {minimum: 5}
+  validates :nickname, presence: true, length: {maximum: 40}
+  validates :birthday, presence: true
+  validates :password, format: { with: /\A[a-zA-Z\d]+\z/, message:"include both letters and numbers" }, length: {minimum: 5}
   validates :birthday, presence: true
   with_options presence: true, format: { with: /\A[一-龥]+\z/ } do
-    validates :last_name, format: { message: "Last name Full-Width characters" }
-    validates :first_name, format: { message: "First name Full-Width characters" }
+    validates :last_name, format: { message: "Full-Width characters" }
+    validates :first_name, format: { message: "Full-Width characters" }
   end
   with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
-    validates :last_furigana, format: { message: "Last furigana Full-width katakana charaters" }
-    validates :first_furigana, format: { message: "First furigana Full-width katakana charaters" }
+    validates :last_furigana, format: { message: "Full-width katakana charaters" }
+    validates :first_furigana, format: { message: "Full-width katakana charaters" }
   end
 end
