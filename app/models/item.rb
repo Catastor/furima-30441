615e43e1927c79e41444453dee96c432e2b74331
Catastor <71878166+Catastor@users.noreply.github.com>
2020-10-28 15:57:48 +0900

    itemモデルにバリデーション、アソシエーションを記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,24 @@
 class Item < ApplicationRecord
+
+  with_options presence: true do
+    validates :name
+    validates :info
+    validates :image
+  end
+
+  validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  
+  validates :category_id, :status_id, :delivery_charge_id, :shipment_source_id, :date_of_shipment_id, numericality: { other_than: 1 }
+
   belongs_to :user
   has_many   :comments
   has_one    :buyer
+  has_one_attached :image
 
-  validates :name, :info, :price, presence: true
-  validates :category_id, :status_id, :delivery_charge_id, :shipment_source_id, :date_of_shipment, numericality: { other_than: 1 }
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :delivery_charge
+  belongs_to_active_hash :shipment_source
+  belongs_to_active_hash :date_of_shipment
 end
