e1284bba19b6b77ecde72e22f1b0ab547a7a8492
Catastor <71878166+Catastor@users.noreply.github.com>
2020-10-28 20:17:16 +0900

    format → numericality

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
     validates :price
   end
 
-  validates :price, format: { with: /\A[0-9]+\z/, message: 'Half-width number' }
+  validates :price, numericality: { with: /\A[0-9]+\z/, message: 'Half-width number' }
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'Out of setting range'}
 
   validates :category_id, :status_id, :delivery_charge_id, :shipment_source_id, :date_of_shipment_id, numericality: { other_than: 1 , message: 'Select'}
