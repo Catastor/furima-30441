3db976e880c9cb303444e160fd4cfe404167e44e
Catastor <71878166+Catastor@users.noreply.github.com>
2020-10-29 19:04:12 +0900

    JavaScriptの実装

---
+++ app/javascript/items.js
@@ -0,0 +1,10 @@
+window.addEventListener('load',() => {
+  const priceCalc = document.getElementById("item-price");
+  priceCalc.addEventListener("input",(e) => {
+    const inputValue = priceCalc.value;
+    const salesCom = document.getElementById("add-tax-price");
+    salesCom.innerHTML = Math.floor(inputValue * 0.1);
+    const salesProf = document.getElementById("profit");
+    salesProf.innerHTML = Math.floor(inputValue - inputValue * 0.1 );
+  });
+});
