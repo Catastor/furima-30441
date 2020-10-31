dd4736c1f9af402536b65c706c71852f66441897
Catastor <71878166+Catastor@users.noreply.github.com>
2020-11-01 02:55:48 +0900

    showアクションの追記
    authenticate_user!からshowアクションを削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :show]
+  before_action :authenticate_user!, only: [:new]
 
   def index
     @items = Item.order('created_at DESC')
@@ -19,13 +19,19 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def show
-    # @item = Item.all
-  # end
+  def show
+    @item = Item.find(params[:id])
+    @category = Category.data.detect{ |s| s[:id] == @item.category_id }
+    @status = Status.data.detect{ |s| s[:id] == @item.status_id }
+    @delivery_charge = DeliveryCharge.data.detect{ |s| s[:id] == @item.delivery_charge_id }
+    @shipment_source = ShipmentSource.data.detect{ |s| s[:id] == @item.shipment_source_id }
+    @date_of_shipment = DateOfShipment.data.detect{ |s| s[:id] == @item.date_of_shipment_id }
+  end
 
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :info, :price, :category_id, :status_id, :delivery_charge_id, :shipment_source_id, :date_of_shipment_id).merge(user_id: current_user.id)
   end
+
 end
