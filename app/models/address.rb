4fed3a9081a64b71496ad939a8cd5b9777e8c049
Catastor <71878166+Catastor@users.noreply.github.com>
2020-11-07 17:36:58 +0900

    addressモデルの修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,9 @@
 class Address < ApplicationRecord
-  belongs_to :buyer
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :buyer, dependent: :destroy
+  belongs_to_active_hash :shipment_source
 
   validates :postal_code, :municipalities, :house_number, :phone_number, presence: true
   validates :shipment_source_id, numericality: { other_than: 1 }
+  validate :building
 end
