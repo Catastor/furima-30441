94a5dbedfe49cb2c8ace9a97377b009f9091b8b5
Catastor <71878166+Catastor@users.noreply.github.com>
2020-11-07 17:49:28 +0900

    テーブルにカラムを追加
    カラムを追加するためのマイグレーションファイルを作成し、add_column、add_referenceでカラムを追加した後、追加のためのマイグレーションファイルを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_27_021150) do
+ActiveRecord::Schema.define(version: 2020_11_06_095556) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -80,6 +80,15 @@ ActiveRecord::Schema.define(version: 2020_10_27_021150) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "item_id", null: false
+    t.bigint "buyer_id", null: false
+    t.index ["buyer_id"], name: "index_orders_on_buyer_id"
+    t.index ["item_id"], name: "index_orders_on_item_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -104,4 +113,6 @@ ActiveRecord::Schema.define(version: 2020_10_27_021150) do
   add_foreign_key "buyers", "users"
   add_foreign_key "comments", "items"
   add_foreign_key "comments", "users"
+  add_foreign_key "orders", "buyers"
+  add_foreign_key "orders", "items"
 end
