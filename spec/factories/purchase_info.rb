2e3cbdaa5efe3b94aba884b3f08473fc4f78f89d
Catastor <71878166+Catastor@users.noreply.github.com>
2020-11-09 19:06:07 +0900

    factorybotの追記

--- spec/factories/purchase_info.rb
+++ spec/factories/purchase_info.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :purchase_info do
+    user_id {1}
+    item_id {1}
     token { 'tok_abcdefghijk00000000000000000' }
     postal_code { '123-1234' }
     shipment_source_id { 2 }
