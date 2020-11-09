99cc724befe622ed1d7731318d7c757752c0f25b
Catastor <71878166+Catastor@users.noreply.github.com>
2020-11-09 20:20:13 +0900

    不要なカラムの削除

--- db/migrate/20201109055708_add_buy_toitems.rb
+++
@@ -1,5 +0,0 @@
-class AddBuyToitems < ActiveRecord::Migration[6.0]
-  def change
-    add_column :items, :buyed, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_09_055708) do
+ActiveRecord::Schema.define(version: 2020_11_09_111401) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -77,7 +77,6 @@ ActiveRecord::Schema.define(version: 2020_11_09_055708) do
     t.integer "shipment_source_id", null: false
     t.integer "date_of_shipment_id", null: false
     t.bigint "user_id", null: false
-    t.string "buyed"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
