39a76912feca7c49af453dcc9aa9957bc1afd7c3
Catastor <71878166+Catastor@users.noreply.github.com>
2020-11-19 20:31:56 +0900

    commentsコントローラーのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'comments/new'
   devise_for :users
   root to: "items#index"
   resources :items do
     resources :buyers
     resources :orders
+    resources :comments
   end
 end
