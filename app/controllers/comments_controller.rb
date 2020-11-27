7c327e382026146912d0f634e0add6fa8af5570c
Catastor <71878166+Catastor@users.noreply.github.com>
2020-11-27 19:08:19 +0900

    commentsコントローラーの修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,11 @@
 class CommentsController < ApplicationController
-  def new
-    @comments = Comment.all
-    @comment = Comment.new 
-  end
 
   def create
-    @message.new(text: params[:message][:text])
+    binding.pry
+    @comment = Comment.new(text: params[:comment][:text])
     if @comment.save
       ActionCable.server.broadcast 'comment_channel', content: @comment
     end
   end
+
 end
